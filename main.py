ef645cffab61f151009d8ecde27a05d3178a11b1
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-12-14 11:39:09 -0500

    added barebones website and support for static files

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, render_template, url_for, request
+app = Flask(__name__, static_folder='static')
 
-@app.route('/')
-def hello_world():
-    return 'Hello world! This is great!'
+@app.route('/home')
+@app.route('/home/<name>')
+def home(name=None):
+    return render_template('index.html', args={"name": name})
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    context = {}
+    if request.method == 'POST':
+        context['name'] = request.form['name']
+    return render_template('form.html', context=context)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
