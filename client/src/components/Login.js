db1343a671b6a5ff254f4802dc4ac99e412d176f
Nithin Vijayakumar <nithin2463@gmail.com>
2021-06-18 22:11:51 -0400

    refactor into separate login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,89 @@
+import { useState, useContext, useCallback, useEffect } from "react";
+import { SocketContext } from "../context/socket";
+
+const Login = ({ username, room, setUsername, setRoom, setEnteredRoom}) => {
+  const socket = useContext(SocketContext);
+  const [validatedMessage, setValidatedMessage] = useState("");
+  const [validated, setValidated] = useState(true);
+
+  const onChangeRoom = (e) => {
+    setRoom(e.target.value);
+  };
+  const onChangeUserName = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if ((room.length === 4) & (username.length !== 0)) {
+      socket.emit("join", { username: username, room: room });
+    } else {
+      alert("Room Code must be 4 characters");
+    }
+  };
+  /**
+   * socket.io - entered_room
+   */
+  const handleEnteredRoom = useCallback((data) => {
+    if (data !== "T") {
+      setValidatedMessage(data);
+      setValidated(false);
+    } else {
+      setEnteredRoom(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    socket.on("entered_room", handleEnteredRoom);
+
+    return () => socket.off("entered_room", handleEnteredRoom);
+  }, [handleEnteredRoom, socket]);
+
+  return (
+    <div className="card mt-5">
+      <div className="card-body">
+        <form onSubmit={(e) => onFormSubmit(e)}>
+          <div className="row m-3 align-items-center">
+            <div className="col-sm-4">
+              <label className="col-form-label fs-2">Username</label>
+            </div>
+            <div className="col-sm-8">
+              <input
+                className={
+                  "form-control form-control-lg " + (!validated && "is-invalid")
+                }
+                value={username}
+                name="username"
+                onChange={(e) => onChangeUserName(e)}
+              />
+              <span style={{ color: "red" }}>{validatedMessage}</span>
+            </div>
+          </div>
+          <div className="row m-3 align-items-center">
+            <div className="col-sm-4">
+              <label className="col-form-label fs-2">Room</label>
+            </div>
+            <div className="col-sm-8">
+              <input
+                className="form-control form-control-lg"
+                value={room}
+                name="room"
+                onChange={(e) => onChangeRoom(e)}
+              />
+            </div>
+          </div>
+          <div className="row m-3">
+            <button
+              className="btn btn-primary btn-lg col-md-4 offset-sm-4"
+              type="submit"
+            >
+              Enter Room
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,7 @@
 import { useState, useContext, useCallback, useEffect } from "react";
 import { SocketContext } from "../context/socket";
 
+import Login from "./Login";
 import MessageBox from "./MessageBox";
 import PlayingArea from "./PlayingArea";
 
@@ -11,24 +12,6 @@ const Main = () => {
   const [enteredRoom, setEnteredRoom] = useState(false);
   const [startedGame, setStartedGame] = useState(false);
   const [gameWinner, setGameWinner] = useState("");
-  const [validatedMessage, setValidatedMessage] = useState("");
-  const [validated, setValidated] = useState(true);
-
-  const onChangeRoom = (e) => {
-    setRoom(e.target.value);
-  };
-  const onChangeUserName = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const onClick = (e) => {
-    e.preventDefault();
-    if ((room.length === 4) & (username.length !== 0)) {
-      socket.emit("join", { username: username, room: room });
-    } else {
-      alert("Room Code must be 4 characters");
-    }
-  };
 
   const onErase = () => {
     setEnteredRoom(false);
@@ -55,23 +38,6 @@ const Main = () => {
     return () => socket.off("game_started", handleStartedGame);
   }, [handleStartedGame, socket]);
 
-  /**
-   * socket.io - entered_room
-   */
-  const handleEnteredRoom = useCallback((data) => {
-    if (data !== "T") {
-      setValidatedMessage(data);
-      setValidated(false);
-    } else {
-      setEnteredRoom(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    socket.on("entered_room", handleEnteredRoom);
-
-    return () => socket.off("entered_room", handleEnteredRoom);
-  }, [handleEnteredRoom, socket]);
 
   /**
    * socket.io - win_game
@@ -90,50 +56,13 @@ const Main = () => {
   return (
     <div className="container-sm" style={{ maxWidth: "100%" }}>
       {!enteredRoom && (
-        <div className="card mt-5">
-          <div className="card-body">
-            <form onSubmit={(e) => onClick(e)}>
-              <div className="row m-3 align-items-center">
-                <div className="col-sm-4">
-                  <label className="col-form-label fs-2">Username</label>
-                </div>
-                <div className="col-sm-8">
-                  <input
-                    className={
-                      "form-control form-control-lg " +
-                      (!validated && "is-invalid")
-                    }
-                    value={username}
-                    name="username"
-                    onChange={(e) => onChangeUserName(e)}
-                  />
-                  <span style={{ color: "red" }}>{validatedMessage}</span>
-                </div>
-              </div>
-              <div className="row m-3 align-items-center">
-                <div className="col-sm-4">
-                  <label className="col-form-label fs-2">Room</label>
-                </div>
-                <div className="col-sm-8">
-                  <input
-                    className="form-control form-control-lg"
-                    value={room}
-                    name="room"
-                    onChange={(e) => onChangeRoom(e)}
-                  />
-                </div>
-              </div>
-              <div className="row m-3">
-                <button
-                  className="btn btn-primary btn-lg col-md-4 offset-sm-4"
-                  type="submit"
-                >
-                  Enter Room
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <Login
+          username={username}
+          setUsername={setUsername}
+          room={room}
+          setRoom={setRoom}
+          setEnteredRoom={setEnteredRoom}
+        />
       )}
 
       {enteredRoom && (
