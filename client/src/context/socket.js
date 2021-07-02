3704f7af0d2b795fb9fc571c97275af80379d159
Nithin Vijayakumar <nithin2463@gmail.com>
2021-07-01 21:29:07 -0400

    fix for dev version

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import socketio from "socket.io-client";
 
-const host =  process.env.NODE_ENV !== 'production' ? "http://localhost" : process.env.REACT_APP_AWS_ADDRESS
+const host =  process.env.NODE_ENV !== 'production' ? "http://localhost:5000" : process.env.REACT_APP_AWS_ADDRESS
 export const socket = socketio.connect(host);
 export const SocketContext = React.createContext();
