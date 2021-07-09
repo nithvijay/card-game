15aaa5df3c970865bd2329c1063e56b820989a2e
Nithin Vijayakumar <nithin2463@gmail.com>
2021-07-08 23:49:17 -0400

    remove old files

--- client/src/context/socket.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import socketio from "socket.io-client";
-
-const host =  process.env.NODE_ENV !== 'production' ? "http://localhost:5000" : process.env.REACT_APP_AWS_ADDRESS
-export const socket = socketio.connect(host);
-export const SocketContext = React.createContext();
--- server/utils/db_utils.py
+++
@@ -1,2 +0,0 @@
-def gen_main_id(main_id_generator):
-    return str(main_id_generator)
