1bbff4cc18f6667e8e57ba7dd5cdb3766b8e4284
Nithin Vijayakumar <nithin2463@gmail.com>
2021-08-03 16:39:36 -0400

    fix directory for scripts

--- aws/code-deploy/scripts/afterInstall.sh
+++ aws/code-deploy/scripts/afterInstall.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 export REACT_APP_AWS_ADDRESS=http://ec2-44-196-116-170.compute-1.amazonaws.com
 
+cd /home/ec2-user
 sudo mv nginx.conf /etc/nginx/nginx.conf
 sudo mv aws/ec2/flask-server.service /etc/systemd/system/flask-server.service
 
 cd client
+npm install
 npm run build 
 rm -r /data/dist
 mv dist/ /data/
 
 cd ../server
+python3 -m venv card-game-env
 source card-game-env/bin/activate
 pip install -r requirements.txt
 
--- aws/code-deploy/scripts/beforeInstall.sh
+++ aws/code-deploy/scripts/beforeInstall.sh
@@ -1,7 +1,1 @@
 #!/usr/bin/bash
-
-cd client
-npm install
-
-cd ../server
-python3 -m venv card-game-env
