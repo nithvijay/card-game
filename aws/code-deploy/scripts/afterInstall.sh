92b9ceb176af46cd4565a3819ad68322205c16bb
Nithin Vijayakumar <nithin2463@gmail.com>
2021-08-03 18:55:44 -0400

    remove unncessary packages

--- aws/code-deploy/scripts/afterInstall.sh
+++ aws/code-deploy/scripts/afterInstall.sh
@@ -4,6 +4,7 @@ export REACT_APP_AWS_ADDRESS=http://ec2-44-196-116-170.compute-1.amazonaws.com
 cd /home/ec2-user
 sudo mv nginx.conf /etc/nginx/nginx.conf
 sudo mv aws/ec2/flask-server.service /etc/systemd/system/flask-server.service
+sudo chown -R ec2-user .
 
 cd client
 npm install
